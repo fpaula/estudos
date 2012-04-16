ce9f464db6832316742e54ca6153335ac4ef81e8
Fred <fpaula@gmail.com>
2012-04-16 10:15:33 -0300

    Incluindo validacao de slug unica

--- aula01/app/models/post.rb
+++ aula01/app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ActiveRecord::Base
+  validates :slug, :uniqueness => true
 end
--- aula01/spec/models/post_spec.rb
+++ aula01/spec/models/post_spec.rb
@@ -2,8 +2,15 @@ require 'spec_helper'
 
 describe Post do
   describe :titulo do
-    before { @titulo = 'Teste titulo'  }
+    before { @titulo = 'Teste titulo' }
     subject { Post.create(:titulo => @titulo) }
     its(:titulo) { should eq @titulo }
+  end
+  
+  describe :slug do
+    before { @post = Post.create!(:slug => 'slug1') }
+    it "doesn't save with an existing slug" do
+      Post.create(:slug => @post.slug).should_not be_valid
+    end
   end
 end

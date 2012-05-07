69fdc76195d55f4a934282dc1ec91ccf47f31dbc
Fred <fpaula@gmail.com>
2012-05-07 08:59:53 -0300

    Custom matchers

--- aula01/spec/models/post_spec.rb
+++ aula01/spec/models/post_spec.rb
@@ -5,12 +5,20 @@ describe Post do
     before { @titulo = 'Teste titulo' }
     subject { Post.create(:titulo => @titulo) }
     its(:titulo) { should eq @titulo }
+    its(:titulo) { should_not be_slugabble }
+    it { should be_activerecord }
+    its(:titulo) { should_not be_activerecord }
   end
   
   describe :slug do
     before { @post = Post.create!(:slug => 'slug1') }
+    subject { @post }
     it "doesn't save with an existing slug" do
       Post.create(:slug => @post.slug).should_not be_valid
     end
+    
+    its(:slug) { should be_slugabble }
+    
+    it { "".should_not be_slugabble }
   end
 end
